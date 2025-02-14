69084761dd827ded04defa6a9a14e6424121d3d8
Hrudhey <hrudhey@gmail.com>
2025-02-14 13:02:19 +0530

    frontend url changed in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe= new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from front end
 const placeOrder= async(req,res)=>{
 
-    const frontend_url= 'http://localhost:5174';
+    const frontend_url= 'https://plant-website-frontend.onrender.com/';
      
     try {
         const newOrder= new orderModel({
